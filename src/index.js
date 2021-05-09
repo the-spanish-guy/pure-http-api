c1cf82a814a63427982a20069ef0ddd15ebd878a
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2021-05-09 20:47:52 -0300

    feat: create route users and add mock

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const http = require("http");
 
+const users = require("./mocks/user");
+
 const PORT = 3000;
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-type": "text/html" });
-  response.end("<h1>Ola Mundo</h1>");
+  console.log(`Request method: ${request.method} on Endpoint: ${request.url}`);
+
+  if (request.url === "/users" && request.method === "GET") {
+    response.writeHead(200, { "Content-type": "application/json" });
+    response.end(JSON.stringify(users));
+  } else {
+    response.writeHead(404, { "Content-type": "text/html" });
+    response.end(`Cannot ${request.method} ${request.url}`);
+  }
 });
 
 server.listen(PORT, () => console.log(`listen on port ${PORT}`));
---
+++ src/mocks/user.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    nome: "Usuario 1",
+  },
+  {
+    id: 2,
+    nome: "Usuario 2",
+  },
+  {
+    id: 3,
+    nome: "Usuario 3",
+  },
+];
