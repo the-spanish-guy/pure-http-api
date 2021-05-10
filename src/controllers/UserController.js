2ccf3daf0d32d35577861ccf4b1123935e73d253
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2021-05-09 21:00:58 -0300

    feat: add suport for query params

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,7 +2,17 @@ const users = require("../mocks/user");
 
 module.exports = {
   listUsers(request, response) {
+    const { order } = request.query;
+    console.log(request.query);
+
+    const sortedUsers = users.sort((a, b) => {
+      if (order === "desc") {
+        return a.id < b.id ? 1 : -1;
+      }
+      return a.id > b.id ? 1 : -1;
+    });
+
     response.writeHead(200, { "Content-type": "application/json" });
-    response.end(JSON.stringify(users));
+    response.end(JSON.stringify(sortedUsers));
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 const http = require("http");
-
+const url = require("url");
 const routes = require("./routes");
 
 const PORT = 3000;
 
 const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method} on Endpoint: ${request.url}`);
+  const parsedUrl = url.parse(request.url, true);
+  console.log(parsedUrl);
 
   const route = routes.find(
     (routeObj) =>
-      routeObj.method === request.method && routeObj.endpoint === request.url
+      routeObj.method === request.method &&
+      routeObj.endpoint === parsedUrl.pathname
   );
 
   if (route) {
+    request.query = parsedUrl.query;
     route.handler(request, response);
   } else {
     response.writeHead(404, { "Content-type": "text/html" });
